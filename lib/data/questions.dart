cf86ead043b40c0d42935016feb07b25de6fb123
HammadM48 <hammadmahmood484@gmail.com>
2025-09-29 22:43:14 -0400

    Stubbed out questions screen. Created quiz Questions. Modified question model.

---
+++ lib/data/questions.dart
@@ -0,0 +1,75 @@
+import 'package:quiz_app/models/quiz_questions.dart';
+
+const questions = [
+    QuizQuestion("Who is the real identity of the 'Bay Harbor Butcher'?",
+    [
+      "Dexter Morgan",
+      "James Doakes",
+      "Brian Moser",
+      "Miguel Prado",
+    ],
+  ),
+
+  QuizQuestion("Who is the real identity of the 'Ice Truck Killer'?",
+    [
+      "Brian Moser",
+      "Arthur Mitchell",
+      "Travis Marshall",
+      "Oliver Saxon",
+    ],
+  ),
+
+  QuizQuestion("Who is the real identity of the 'Skinner'?",
+    [
+      "George King",
+      "Isaak Sirko",
+      "Jordan Chase",
+      "Dexter Morgan",
+    ],
+  ),
+
+  QuizQuestion("Who is the real identity of the 'Trinity Killer'?",
+    [
+      "Arthur Mitchell",
+      "Brian Moser",
+      "Oliver Saxon",
+      "Travis Marshall",
+    ],
+  ),
+
+  QuizQuestion("Who is the real identity of the 'Barrel Girl Gang' leader?",
+    [
+      "Jordan Chase",
+      "George King",
+      "Isaak Sirko",
+      "Arthur Mitchell",
+    ],
+  ),
+
+  QuizQuestion("Who is the real identity of the 'Doomsday Killer'?",
+    [
+      "Travis Marshall",
+      "James Doakes",
+      "Brian Moser",
+      "Oliver Saxon",
+    ],
+  ),
+
+  QuizQuestion("Who is the real identity of the 'Ukrainian Mobster'?",
+    [
+      "Isaak Sirko",
+      "Arthur Mitchell",
+      "George King",
+      "Jordan Chase",
+    ],
+  ),
+
+  QuizQuestion("Who is the real identity of the 'Brain Surgeon'?",
+    [
+      "Oliver Saxon",
+      "Travis Marshall",
+      "Brian Moser",
+      "George King",
+    ],
+  ),
+];
